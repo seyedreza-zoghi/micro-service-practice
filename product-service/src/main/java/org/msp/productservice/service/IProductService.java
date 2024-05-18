7dfe2df1c5cf3d68cad09eb56b9e4c5c07fd0fa8
seyedreza.zoghi <seyedreza.zoghi@gmail.com>
2024-05-18 21:48:04 +0330

    feat : add service crud

--- product-service/src/main/java/org/msp/productservice/service/IProductService.java
+++ product-service/src/main/java/org/msp/productservice/service/IProductService.java
@@ -6,4 +6,8 @@ import java.util.List;
 
 public interface IProductService {
     List<Product> getProducts();
+    Product getProductById(Long id);
+    Product createProduct(Product product);
+    Product updateProduct(Long id, Product productDetails);
+    void deleteProduct(Long id);
 }
