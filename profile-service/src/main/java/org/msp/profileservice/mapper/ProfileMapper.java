eaab8b4c3b7f2e4a7b03e828e5b04993c485b366
r.zoghi <seyedreza.zoghi@gmail.com>
2024-08-21 10:07:32 +0330

    feat(mapper): add new mappers

---
+++ profile-service/src/main/java/org/msp/profileservice/mapper/ProfileMapper.java
@@ -0,0 +1,10 @@
+package org.msp.profileservice.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
+public interface ProfileMapper {
+
+}
