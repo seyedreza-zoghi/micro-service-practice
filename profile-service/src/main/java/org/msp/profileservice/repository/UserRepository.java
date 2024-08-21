0d3e37d87ad1239e90a13c1497e06eae36376ce3
r.zoghi <seyedreza.zoghi@gmail.com>
2024-08-21 10:07:11 +0330

    feat(repo): add new repo

---
+++ profile-service/src/main/java/org/msp/profileservice/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package org.msp.profileservice.repository;
+
+import org.msp.profileservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+    Optional<User> findByUsername(String username);
+}
