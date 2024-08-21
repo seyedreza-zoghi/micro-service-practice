0291c09a1e69521c34caef0247b37e897c47d476
r.zoghi <seyedreza.zoghi@gmail.com>
2024-08-21 10:10:06 +0330

    feat(controller): profile and user controller

--- profile-service/src/main/java/org/msp/profileservice/controller/ProfileController.java
+++ profile-service/src/main/java/org/msp/profileservice/controller/ProfileController.java
@@ -1,21 +1,16 @@
 package org.msp.profileservice.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.msp.profileservice.external.ProductClient;
-import org.msp.profileservice.external.modelResponse.ProductResponse;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class ProfileController {
 
-    private final ProductClient productClient;
-    @GetMapping("/profiles")
-    public List<ProductResponse> profiles() {
-        List<ProductResponse> products = productClient.getProducts();
-        return products;
-    }
+//    private final ProductClient productClient;
+//    @GetMapping("/profiles")
+//    public List<ProductResponse> profiles() {
+//        List<ProductResponse> products = productClient.getProducts();
+//        return products;
+//    }
 }
