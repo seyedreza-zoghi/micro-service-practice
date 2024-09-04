b98441d54b56094497a1d4f35038a2ea76fefa79
seyedreza.zoghi <seyedreza.zoghi@gmail.com>
2024-09-04 21:32:25 +0330

    feat : override create and get all for show openAi

--- security-service/src/main/java/org/msp/securityservice/controller/UserController.java
+++ security-service/src/main/java/org/msp/securityservice/controller/UserController.java
@@ -4,9 +4,7 @@ package org.msp.securityservice.controller;
 import msp.core.controller.GenericController;
 import msp.core.service.GenericService;
 import org.msp.securityservice.model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,8 +16,15 @@ public class UserController extends GenericController<User,Long> {
         super(service);
     }
 
-    @GetMapping("/test")
-    public List<User> test() {
-        return null;
+    @Override
+    @PostMapping("/save")
+    public User create(@RequestBody User entity) {
+        return super.create(entity);
+    }
+
+    @Override
+    @GetMapping("/getAll")
+    public List<User> getAll() {
+        return super.getAll();
     }
 }
