808bcf1ca982933c245f4815ecfd7b5b8fa06f49
seyedreza.zoghi <seyedreza.zoghi@gmail.com>
2024-09-05 17:45:41 +0330

    feat : add JsonProperty

--- profile-service/src/main/java/org/msp/profileservice/external/modelResponse/ProductResponse.java
+++ profile-service/src/main/java/org/msp/profileservice/external/modelResponse/ProductResponse.java
@@ -1,11 +1,14 @@
 package org.msp.profileservice.external.modelResponse;
 
-import jakarta.persistence.Version;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class ProductResponse {
+    @JsonProperty
     private Long id;
+    @JsonProperty
     private String name;
+    @JsonProperty
     private String price;
 }
