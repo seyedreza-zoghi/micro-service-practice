acc3d6be695a9ddbe9c8c3a4a5e7095f9e6c894a
r.zoghi <seyedreza.zoghi@gmail.com>
2024-09-07 09:30:08 +0330

    feat: edit models erd

--- security-service/src/main/java/org/msp/securityservice/model/Permissions.java
+++ security-service/src/main/java/org/msp/securityservice/model/Permissions.java
@@ -1,8 +1,6 @@
 package org.msp.securityservice.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,19 +8,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import msp.core.model.GenericBaseEntity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
-@Table
+@Table(name = "permissions")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Permissions extends GenericBaseEntity<Long> {
-
-    @Column(name = "name", nullable = false, unique = true)
     private String name;
-    @ManyToMany(mappedBy = "permissions")
-    private List<Role> roles = new ArrayList<>();
+    private boolean requiresVerification;
 }
